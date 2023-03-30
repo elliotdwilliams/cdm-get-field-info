06d6e31141f824e776c4c8dd2fc484c85f613600
Elliot Williams <elliotdwilliams@gmail.com>
2023-03-30 16:45:39 -0500

    Add ability to get collection list from API

--- cdm-get-field-info.py
+++ cdm-get-field-info.py
@@ -1,7 +1,22 @@
 import requests
 
-# Open a file to get the list of collection aliases, then split that list into separate lines
-alias_list = open("alias-test.txt", "r", encoding="utf-8").readlines()
+# Make an API request to get collection list
+collection_list_url = "https://digital.utsa.edu/digital/bl/dmwebservices/index.php?q=dmGetCollectionList/json"
+collection_response = requests.get(collection_list_url)
+
+# Create an empty list to hold collection aliases
+alias_list = []
+
+# Loop through collection list and add alias to list
+for collection in collection_response.json():
+    alias = collection["alias"].lstrip("/")
+    #print(alias)
+    alias_list.append(alias)
+
+print(alias_list)
+
+# Alternate method: Open a file to get the list of collection aliases, then split that list into separate lines
+# alias_list = open("alias-test.txt", "r", encoding="utf-8").readlines()
 
 # Loop through each line in the list of aliases
 for line in alias_list:
