5ec3090b9a6accecfdf5546e180f378b815e667c
Elliot Williams <elliotdwilliams@gmail.com>
2023-04-04 14:05:02 -0500

    Minor code cleanup

--- cdm-get-field-info.py
+++ cdm-get-field-info.py
@@ -1,11 +1,16 @@
+'''
+Queries the CONTENTdm Server API for a list of all collections in a repository,
+then downloads the field information for each collection as an XML file.
+'''
+
 import requests
 
 # Set base URL to CONTENTdm website URL
-base_URL = "https://digital.utsa.edu"
+BASE_URL = "https://digital.utsa.edu"
 
 # Make an API request to get collection list
-collection_list_url = base_URL + "/digital/bl/dmwebservices/index.php?q=dmGetCollectionList/json"
-collection_response = requests.get(collection_list_url)
+collection_list_url = BASE_URL + "/digital/bl/dmwebservices/index.php?q=dmGetCollectionList/json"
+collection_response = requests.get(collection_list_url, timeout=5)
 
 # Create an empty list to hold collection aliases
 alias_list = []
@@ -13,7 +18,7 @@ alias_list = []
 # Loop through collection list and add alias to list
 for collection in collection_response.json():
     alias = collection["alias"].lstrip("/")
-    #print(alias)
+    # print(alias)
     alias_list.append(alias)
 
 # Alternate method: Open a file to get the list of collection aliases, then split that list into separate lines
@@ -25,9 +30,9 @@ for line in alias_list:
     print(alias)
 
     # Make an API request for the collection
-    collection_url = base_URL + "/digital/bl/dmwebservices/index.php?q=dmGetCollectionFieldInfo/" + alias + "/xml"
-    response = requests.get(collection_url)
-    print(response.status_code) # Print status code for error checking
+    collection_url = BASE_URL + "/digital/bl/dmwebservices/index.php?q=dmGetCollectionFieldInfo/" + alias + "/xml"
+    response = requests.get(collection_url, timeout=5)
+    print(response.status_code)  # Print status code for error checking
 
     # Save the request output to file
     with open(alias + ".xml", mode="w", encoding="utf-8") as f:
